8b7e038104a5285dbb2419410e4881f388a1133f
christian <christian.diaz2101@alumnos.ubiobio.cl>
2025-11-26 22:07:01 -0300

    arreglitos para endpoints libros

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,6 +1,26 @@
+import { IsString, IsOptional } from 'class-validator';
+
 export class CreateBookDto {
+  @IsString()
   title: string;
-  author: string;
-  genre: string;
+
+  @IsString()
+  @IsOptional()
+  synopsis?: string;
+
+  @IsString()
+  @IsOptional()
+  cover_image_url?: string;
+
+  @IsString()
+  @IsOptional()
+  author?: string;
+
+  @IsString()
+  @IsOptional()
+  genre?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
 }
